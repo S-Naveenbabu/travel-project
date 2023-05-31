2fe944878753370f3f811e8821b785ffd6bba474
S-Naveenbabu <naveenbabusamineni@gmail.com>
2023-05-31 15:48:41 +0530

    init

---
+++ travel project/myTMS/urls.py
@@ -0,0 +1,13 @@
+''' myTMS/urls.py(project's urls.py) '''
+
+from django.contrib import admin
+from django.urls import path, include
+
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('loginmodule/', include('loginmodule.urls')),
+    path('SignupApp/', include('SignupApp.urls')),
+    path('BookTicketApp/', include('BookTicketApp.urls')),
+    path('PaymentApp/', include('PaymentApp.urls')),
+]
